53a997544f93e3de8950b2cf8a75632329518ed6
jash215 <58883072+jd-215@users.noreply.github.com>
2023-12-09 20:33:08 +0530

    changes added

--- Product of Array Except Self.ts
+++ Product of Array Except Self.ts
@@ -1,10 +1,7 @@
-// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
-
-// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
-
-// You must write an algorithm that runs in O(n) time and without using the division operation.
-
-//Slolution of the code
+//  Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+//  The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+//  You must write an algorithm that runs in O(n) time and without using the division operation.
+//  Slolution of the code
 
 function productExceptSelf(nums: number[]): number[] {
         
